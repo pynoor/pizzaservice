613ea9e3bfe8e63e9599f906174606925f8204ae
pynoor <32771762+pynoor@users.noreply.github.com>
2018-07-23 23:19:17 +0200

    Connect to PostgrSQL Database

--- pizzaservice/pizzaservice/settings.py
+++ pizzaservice/pizzaservice/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'pizzaservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pizzaservice',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
