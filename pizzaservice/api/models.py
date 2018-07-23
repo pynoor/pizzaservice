bbca0a10b4c43a90d4918a4e44dcc0ccdd5a1536
pynoor <32771762+pynoor@users.noreply.github.com>
2018-07-23 23:42:29 +0200

    Add Order model, make test pass

--- pizzaservice/api/models.py
+++ pizzaservice/api/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Order(models.Model):
-    pass
+
+    pizza_id = models.IntegerField(primary_key=1)
+    pizza_size = models.IntegerField()
+    customer_name = models.CharField(max_length=50)
+    customer_address = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.pizza_id)
