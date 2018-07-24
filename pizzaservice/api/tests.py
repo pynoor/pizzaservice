c627d99e5ea6d5c4ddc3f37e154903211fbb1383
pynoor <32771762+pynoor@users.noreply.github.com>
2018-07-24 11:17:18 +0200

    Add ViewTestCase Class

--- pizzaservice/api/tests.py
+++ pizzaservice/api/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Order
+from rest_framework import status
+from rest_framework import APIClient
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 class OrderTestCase(TestCase):
@@ -7,8 +10,8 @@ class OrderTestCase(TestCase):
     def setUp(self):
         self.pizza_id = 1
         self.pizza_size = 30
-        self.customer_name = "Jobo"
-        self.customer_address = "Weirdroad 17, 15533 Outerspace, Universe"
+        self.customer_name = 'Jobo'
+        self.customer_address = 'Weirdroad 17, 15533 Outerspace, Universe'
         self.order = Order(pizza_id=self.pizza_id, pizza_size=self.pizza_size,
         customer_name=self.customer_name, customer_address=self.customer_address)
 
@@ -17,3 +20,17 @@ class OrderTestCase(TestCase):
         self.order.save()
         new_count = Order.objects.count()
         self.assertNotAlmostEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.order_data = {'pizza_id': 1, 'pizza_size': 30, 'customer_name': 'Jobo', 'customer_adress': 'Weirdroad 17, 15533 Outerspace, Universe'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.order_data,
+            format='json'
+        )
+
+    def test_api_can_create_an_order(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
