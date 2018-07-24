684134276d43f063d9e0fdbd6edb0f3e05a0e5dd
pynoor <32771762+pynoor@users.noreply.github.com>
2018-07-24 12:12:08 +0200

    Connect DetailsView to url

--- pizzaservice/api/urls.py
+++ pizzaservice/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import OrderCreateView
+from .views import OrderCreateView, OrderDetailsView
 
 urlpatterns = [
-    url(r'^$', OrderCreateView.as_view(), name='create')
+    url(r'^$', OrderCreateView.as_view(), name='create'),
 
+    url(r'orders/(?P<pk>[0-9]+)/$', OrderDetailsView.as_view(), name="details"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
