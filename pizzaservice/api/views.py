cd269e294a8bf71a40dc687b99868834f3e2a7e3
pynoor <32771762+pynoor@users.noreply.github.com>
2018-07-24 11:44:32 +0200

    Add CreateView

--- pizzaservice/api/views.py
+++ pizzaservice/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import OrderSerializer
+from .models import Order
 
 # Create your views here.
+
+class OrderCreateView(generics.ListCreateAPIView):
+    """This class defines the create behaviour of our rest api."""
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
