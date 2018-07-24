993b1d1319169f3f8023159a80b76e8ad12e5754
pynoor <32771762+pynoor@users.noreply.github.com>
2018-07-24 15:13:14 +0200

    Remove comments

--- pizzaservice/api/serializers.py
+++ pizzaservice/api/serializers.py
@@ -7,7 +7,3 @@ class OrderSerializer(serializers.ModelSerializer):
 
         model = Order
         fields = ('id', 'pizza_id', 'pizza_size', 'customer_name', 'customer_address')
-
-    #Attention! Can't just add 'id' fiel here ..
-    #Error : django.core.exceptions.ImproperlyConfigured: Field name `id` is not valid for model `Order`.
-    #This is because I have manually set a primary key so the automatic id is not added. Gotta fix that.
